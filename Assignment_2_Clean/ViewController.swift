2cd0a52cb87aafd3c79ff612227e11e21e37a407
ArtemBatkov <artembatkov2015@gmail.com>
2023-01-24 10:19:59 -0500

    Initial Commit

---
+++ Assignment_2_Clean/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment_2_Clean
+//
+//  Created by user233573 on 1/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
